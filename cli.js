107007d81b77cf05e20fd695b614f669fd9b5311
garywill <garywill@disroot.org>
2024-03-02 13:44:51 +0800

    修改路径

--- cli.js
+++ cli.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 eval(fs.readFileSync('common.js').toString());
 eval(fs.readFileSync('init.js').toString());
 
-eval(fs.readFileSync("unicode-data/unicode-data-blocks.js").toString());
-eval(fs.readFileSync("unicode-data/unicode-data-ages.js").toString());
-eval(fs.readFileSync("unicode-data/unicode-data-Cc.js").toString());
-eval(fs.readFileSync("unicode-data/unicode-data-Mn.js").toString());
+eval(fs.readFileSync("data/unicode-data/unicode-data-blocks.js").toString());
+eval(fs.readFileSync("data/unicode-data/unicode-data-ages.js").toString());
+eval(fs.readFileSync("data/unicode-data/unicode-data-Cc.js").toString());
+eval(fs.readFileSync("data/unicode-data/unicode-data-Mn.js").toString());
 eval(fs.readFileSync("ucd.js").toString());
 
-eval(fs.readFileSync("summary-data/summary-data-map2.js").toString());
+eval(fs.readFileSync("data/summary-data/summary-data-map2.js").toString());
 
 eval(fs.readFileSync("unusual_conditions.js").toString());
 eval(fs.readFileSync("checkessay.js").toString());
--- sync_opencc.sh
+++ sync_opencc.sh
@@ -23,16 +23,16 @@ TWVariants.txt
 
 for ((i=0; i<${#opencc_file_list[@]}; i++ ))
 do
-    sha1sum opencc-data/${opencc_file_list[i]}
-    wget https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/${opencc_file_list[i]} -O opencc-data/${opencc_file_list[i]} || exit 1
-    #curl https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/${opencc_file_list[i]} > opencc-data/${opencc_file_list[i]}
+    sha1sum data/opencc-data/${opencc_file_list[i]}
+    wget https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/${opencc_file_list[i]} -O data/opencc-data/${opencc_file_list[i]} || exit 1
+    #curl https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/${opencc_file_list[i]} > data/opencc-data/${opencc_file_list[i]}
 
 
     git config user.name bot
     git config user.email bot@github.bot.none
 
-    sha1sum opencc-data/${opencc_file_list[i]}
-    git add opencc-data/${opencc_file_list[i]}
+    sha1sum data/opencc-data/${opencc_file_list[i]}
+    git add data/opencc-data/${opencc_file_list[i]}
     git diff
     git commit -m "sync opencc  ${opencc_file_list[i]}"
     #git log
@@ -40,16 +40,16 @@ do
 done
 
 pushd .
-cd opencc-data
+cd data/opencc-data
 node pre.js  || exit 1
-git add -u opencc-data-*.js
+git add -u data/opencc-data-*.js
 git commit -m "run opencc pre.js"
 popd
 
 pushd .
-cd summary-data
+cd data/summary-data
 node pre.js   || exit 1
-git add -u summary-data-*.js
+git add -u data/summary-data-*.js
 git commit -m "run summary pre.js"
 popd
 
