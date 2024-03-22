3df3150af8d5c929cbcf9dfbe9e9b12c1bda4361
garywill <garywill@disroot.org>
2024-03-23 00:23:04 +0800

    build.sh : 清除出错忽略

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
-rm -rf dist-vccrlib/*  || exit 1
+mkdir -p dist-vccrlib
+rm -rf dist-vccrlib/* 
 ( echo -e "\n\nGoing to build 'vccrlib'...\n\n" ; env buildtarget='vccrlib' npx webpack  )  || exit 1
 
-rm -rf dist-webtool/*  || exit 1
+mkdir -p dist-webtool
+rm -rf dist-webtool/*  
 ( echo -e "\n\nGoing to build 'webtool'...\n\n" ; env buildtarget='webtool' npx webpack  )  || exit 1
 
