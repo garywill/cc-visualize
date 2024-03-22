ac6c7a3ae1c1ed5310fdcfefd16b7db873d737f7
garywill <garywill@disroot.org>
2024-03-23 00:00:00 +0800

    move escapeHtml() to webjs

--- common.js
+++ common.js
@@ -10,15 +10,3 @@ if (isNode)
 if (isWeb)
     console.log("isWeb");
 
-function escapeHtml(unsafe)
-{
-    if (typeof(unsafe) !== "string" )
-        return escapeHtml( toString(unsafe) );
-    
-    return unsafe
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
--- js_web/common.js
+++ js_web/common.js
@@ -103,3 +103,15 @@ function removeNode(node)
     node.parentNode.removeChild(node);
 }
 
+function escapeHtml(unsafe)
+{
+    if (typeof(unsafe) !== "string" )
+        return escapeHtml( toString(unsafe) );
+    
+    return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
